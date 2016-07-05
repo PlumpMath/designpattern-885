3ae863c708ff9f8227c7ba4ea9a349f27ff2cb4e
libing <lb_special@163.com>
2016-07-05 13:29:32 +0800

    add Test.java

---
+++ src/test/java/cn/com/tcsl/test/Test.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2016 libing. All rights reserved.
+ */
+package cn.com.tcsl.test;
+
+/**
+ * <P>
+ * Description:
+ * </p>
+ * @author libing
+ * @version 1.0
+ * @Date 2016年7月5日下午1:29:05
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
